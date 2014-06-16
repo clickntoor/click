014698f6ec78428339a5c5977856fe25e4225484
Nagarjuna <ynagarjuna2012@gmail.com>
2014-06-16 19:20:08 +0530

    Initial Site

---
+++ app/views/home.blade.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+   	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>clickntoor|Hassle free trips at your finger trips </title>
+	<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<!-- Latest compiled and minified JavaScript -->
+		 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+		<link href="//fonts.googleapis.com/css?family=Open+Sans:300italic,300,400italic,400,600italic,600,700italic,700,800italic,800" rel="stylesheet" type="text/css">
+
+		<link rel="short cut" href="/ico/favicon.ico">
+		{{stylesheet_link_tag()}}
+		{{javascript_include_tag()}}
+</head>
+<body>
+	
+			<div class="home-inner home-image">
+				<div class="mask">
+
+				   <div class="container">
+				   		<div class="row">
+				   		<div class="col-md-12 col-xs-12 col-sm-12">
+				   		<div class="logo left">
+				   			<a href="/">{{HTML::image('assets/logobot.png','logo',array('class'=>'img-responsive'))}}</a>
+				 		<div class="top_right">
+							<div class="login-area">
+								<div class="join-our-team">
+									Join with us.
+								</div>
+							</div>
+				   		</div>
+				   		</div>
+				   		</div>
+				   		<div class=" col-md-12 col-sm-12 col-xs-12 add-image">
+				   		<div class="welcome-text">
+				   			<h1>Discover the great places to hangout with your family and freinds around Bangalore  </h1>
+				   		</div>
+						</div>
+				   		</div>
+				   </div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+		<div class="hp-add">
+			<div class="hp-add-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 col-sm-12 col-xs-12">
+							<h2 class="hp-add-text">Sign up to unlock the clickntoor</h2>
+						</div> 
+						<div class="col-md-4 col-sm-4 col-xs-4">
+							{{HTML::image('assets/img1.png','Follow Trusted Travellers',array('class'=>'img-responsive'))}}
+							<h4>Follow Travellers around the globe</h4>
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-4">
+							
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-4">
+							
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	<div class="hp-login">
+		<div class="hp-login-wrapper">
+			
+		</div>
+	</div>
+	
+	<footer class="main-footer">
+
+	<div class="brand">
+					<a href="/" class="brandlogo">clickntoor</a>
+				</div>
+		<div class="wrapper">
+			
+			<div class="container">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				
+			<nav>
+				<ul class="primary">
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Join our Team</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">Business</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Advertise</a></li>
+					<li><a href="#">Partner</a></li>
+					<li><a href="#">Invest</a></li>
+				</ul>
+			
+				<ul class="secondary">
+					<li><a href="#">Privacy</a></li>
+					<li><a href="#">Sitemap</a></li>
+					<li><a href="#">Policies</a></li>
+					<li><a href="#">Add your College</a></li>
+				</ul>
+
+			</nav>
+			</div></div>
+		</div>
+	</footer>
+	
+</body>
+</html>
---
+++ app/views/index.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+   		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>clickntoor|Hassle Free Trips at your Finger trips</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<!-- Latest compiled and minified JavaScript -->
+		 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+		<link rel="short cut" href="/ico/favicon.ico">
+		{{stylesheet_link_tag()}}
+		{{javascript_include_tag()}}
+	</head>
+
+
+	<body>
+	<div class="welcome">
+		<div class="welcome_container">
+			<div class="logo_area">
+			
+			{{HTML::image('assets/logo1.png' ,'logo',array('class'=>'img-rounded'))}}	
+		
+			</div>	
+	
+		<div class="contianer">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				
+					<div class="Welcome_text">
+						<h1>Discover the places to hangout with your family and freinds 
+						around  Mangalore  </h1>	
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-lg-12 col-sm-12 ">
+						
+						
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
